ece988b2383bcc13f18c7e9dab103dc0df79e2d5
Andriy Berestovskyy <berestovskyy@gmail.com>
2018-09-20 10:10:28 +0200

    Add license and copyright header to ES and IL

--- IL.js
+++ IL.js
@@ -1,3 +1,23 @@
+/*
+ * IL.js -- WME Validator Localization for Israel
+ * Copyright (C) 2014-2018 gad_m
+ *
+ * This file is part of WME Validator: https://github.com/WMEValidator/
+ *
+ * WME Validator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * WME Validator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with WME Validator. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ".country": "Israel",
 ".author": "gad_m",
 ".updated": "2014-06-30",
