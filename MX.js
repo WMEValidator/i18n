1d88b9797310ca3732dd2c3c1555bf5c6314bce2
Andriy Berestovskyy <berestovskyy@gmail.com>
2018-09-20 10:10:41 +0200

    Add license and copyright header to MX

--- MX.js
+++ MX.js
@@ -1,3 +1,27 @@
+/*
+ * MX.js -- WME Validator Localization for Mexico
+ * Copyright (C) 2014-2018 Carlos Laso
+ *
+ * This file is part of WME Validator: https://github.com/WMEValidator/
+ *
+ * WME Validator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * WME Validator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with WME Validator. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /*************************************************************************
+  * This file falls back to ES (spainish) localization
+  *************************************************************************/
+
 ".country": "Mexico",
 ".updated": "2018-09-19",
 ".author": "carloslaso",
